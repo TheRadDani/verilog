52f5e16b0f7c844c5bd2ea5d6fb392232ff12bb5
TheRadDani <ldanielfch@gmail.com>
2021-08-15 22:32:56 -0600

    check

---
+++ project/decoder2to4_tb.v
@@ -0,0 +1,17 @@
+`timescale 1ns/1ns
+`include "decoder.v"
+module decoder2to4;
+reg [1:0] A;
+wire [3:0] D;
+decoder2to4 UUT(A,D);
+initial begin
+    $dumpfile("ultima.vcd");
+    $dumpvars(0,decoder2to4_tb);
+    A=2'b00;#20;
+    A=2'b00;#20;
+    A=2'b00;#20;
+    A=2'b00;#20;
+    A=2'b00;#20;
+    $display("done");
+end
+endmodule
